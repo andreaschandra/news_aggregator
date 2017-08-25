ace7ef0335a87fb774b90723edeb1a7672308ce6
andreaschandra <phoenix.ichigo@yahoo.com>
2017-08-25 15:27:37 +0700

    domain and main

---
+++ domain.py
@@ -0,0 +1,123 @@
+#HTTP
+bintang_list = [
+          'http://www.bintang.com/celeb/indeks/',
+          'http://www.bintang.com/lifestyle/indeks/',
+          'http://www.bintang.com/beauty/indeks/',
+          'http://www.bintang.com/style/indeks/',
+          'http://www.bintang.com/saur-sepuh/indeks/',
+          'http://www.bintang.com/photo/indeks/',
+          'http://www.bintang.com/video/indeks/',
+          'http://www.bintang.com/unique/indeks/',
+          'http://www.bintang.com/sex-health/indeks/',
+          'http://www.bintang.com/success/indeks/',
+          'http://www.bintang.com/relationship/indeks/',
+          'http://www.bintang.com/food-travel/indeks/',
+         ]
+kompas_list = [
+          'http://indeks.kompas.com/news/',
+          'http://indeks.kompas.com/ekonomi/',
+          'http://indeks.kompas.com/bola/',
+          'http://indeks.kompas.com/tekno/',
+          'http://sains.kompas.com/search/',
+          'http://indeks.kompas.com/entertainment/',
+          'http://indeks.kompas.com/otomotif/',
+          'http://indeks.kompas.com/lifestyle/',
+          'http://indeks.kompas.com/properti/',
+          'http://travel.kompas.com/search/',
+          'http://indeks.kompas.com/edukasi/'
+          ]
+movila_list = [
+          'http://www.muvila.com/film/index',
+          'http://www.muvila.com/tv/index',
+          'http://www.muvila.com/entertainment/index',
+          'http://www.muvila.com/foto/index',
+          'http://www.muvila.com/video/index'
+          ]
+liputan6 = 'http://www.liputan6.com/indeks/'
+okezone = 'http://index.okezone.com/bydate/index/'
+tribunnews = 'http://www.tribunnews.com/index-news?date='
+hipwee = 'http://www.hipwee.com/terbaru/?halaman='
+
+
+
+
+### HTTPS
+
+detik_news    = 'https://news.detik.com/indeks/all/'
+detik_finance = 'https://finance.detik.com/indeks'
+detik_inet    = 'https://inet.detik.com/main/indeks'
+detik_oto     = 'https://oto.detik.com/indeks'
+detik_travel  = 'https://travel.detik.com/indeks'
+detik_hot_sport_sepakbola_list = [
+          'https://hot.detik.com/indeks',
+          'https://sport.detik.com/indeks',
+          'https://sport.detik.com/sepakbola/indeks'
+          ]
+detik_inet_health_list = [
+          'https://food.detik.com/indeks',
+          'https://health.detik.com/indeks'
+          ]
+detik_wolipop = 'https://wolipop.detik.com/index'
+
+jalantikus_list = [
+          'https://jalantikus.com/tips/browse/page/',
+          'https://jalantikus.com/gokil/browse/page/',
+          'https://jalantikus.com/gadgets/browse/page/',
+          'https://jalantikus.com/news/browse/page/'
+          ]
+
+kapanlagi_list = [
+          'https://www.kapanlagi.com/showbiz/selebriti/index2.html',
+          'https://www.kapanlagi.com/showbiz/hollywood/index2.html',
+          'https://www.kapanlagi.com/film/index2.html',
+          'https://www.kapanlagi.com/showbiz/asian-star/index2.html',
+          'https://video.kapanlagi.com/index2.html'
+          ]
+kumparan_list = [
+          'https://api.kumparan.com/v1.0/stories?topic=liputan-khusus&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=viral&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=sepak-bola&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=health&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=kuliner&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=hiburan&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=news&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=ekonomi&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=otomotif&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=politik&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=lifestyle&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=teknologi&page=',
+          'https://api.kumparan.com/v1.0/stories?topic=travel&page=',
+          ]
+otosia_list = [
+          'https://www.otosia.com/berita/index',
+          'https://www.otosia.com/review/index',
+          'https://www.otosia.com/tips/index',
+          ]
+techno_list = [
+          'https://www.techno.id/startup/index',
+          'https://www.techno.id/gadget/index',
+          'https://www.techno.id/apps/index',
+          'https://www.techno.id/social/index',
+          'https://www.techno.id/tech-news/index',
+          'https://www.techno.id/photo/index',
+          'https://www.techno.id/telco/index',
+          'https://www.techno.id/science/index'
+          ]
+vemale_list = [
+          'https://www.vemale.com/parenting/',
+          'https://www.vemale.com/relationship/',
+          'https://www.vemale.com/lifestyle/',
+          'https://www.vemale.com/kuliner/',
+          'https://www.vemale.com/kesehatan/',
+          'https://video.vemale.com/',
+          'https://www.vemale.com/hall-of-fame/',
+          'https://www.vemale.com/dekor-rumah/',
+          'https://www.vemale.com/inspiring/',
+          'https://www.vemale.com/karir/',
+          'https://www.vemale.com/ragam/',
+          'https://www.vemale.com/brand/',
+          'https://www.vemale.com/keuangan/'
+          ]
+tempo = 'https://www.tempo.co/indeks/'
+tirto = 'https://tirto.id/indeks/'
+cnn = 'https://www.cnnindonesia.com/indeks'
---
+++ main.py
@@ -0,0 +1,3 @@
+import domain
+
+print(domain.cnn)	

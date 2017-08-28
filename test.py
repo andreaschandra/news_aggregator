c69751256248c0a69f9ae9cb43d973cd7f3f7b82
andreaschandra <phoenix.ichigo@yahoo.com>
2017-08-28 10:44:14 +0700

    delete test

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-string = 'Intip Latihan Bebe Rexha untuk Malam Puncak HUT SCTV ke-27⁠⁠⁠⁠'
-titleen = string.encode('utf-8')
-
-title = re.sub(r'\\x[a-z0-9]{2}', '', str(titleen))
-
-print(title)

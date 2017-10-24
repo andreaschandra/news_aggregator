e1ff775907ef139b13a8507a4cb9dd25e6dbc623
andreaschandra <phoenix.ichigo@yahoo.com>
2017-10-24 11:23:43 +0700

    update alg

--- server.py
+++ server.py
@@ -1,99 +1,49 @@
-import collections
-import random
 import pandas as pd
-import nltk
-import random
-from nltk.classify import NaiveBayesClassifier
-from nltk.classify.util import accuracy
-from nltk.classify import DecisionTreeClassifier
-from nltk.tokenize import word_tokenize
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 from flask import (
 	Flask,
 	request,
 	render_template)
 
 
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
+df = pd.read_excel('D:/Boombastis/Data/data_aggregator/new.xlsx')
 
-def cleansing(data):
-    doc = []
-    for key in other.keys():
-        for judul in other[key]:
-            title = judul.replace('\n', '') 
-            title = judul.replace('\t', '')
-            stem = stemmer.stem(title)
-            title = nltk.word_tokenize(stem)
-            doc.append((list(title), key))
-    return doc
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df['judul'], 
+                                                    df['num_cat'], 
+                                                    random_state=0)
 
-def document_features(document):
-	document_words = set(document)
-	features = {}
-	for word in document_words:
-	    features['contains(%s)' % word] = (True)
-	return features
+# Fit the CountVectorizer to the training data
+vect = CountVectorizer().fit(X_train)
 
-training_set = pd.read_excel('D:/Boombastis/Data/Data/data.xlsx')
+# transform the documents in the training data to a document-term matrix
+X_train_vectorized = vect.transform(X_train)
 
-data = {'Inspirasi':[],
-       'UnikAneh':[],
-       'Travel':[],
-       'Tips':[],
-       'Hiburan':[],
-       'Olahraga':[],
-       'Trends':[],
-       'Block':[],
-       'Entertainment':[]}
+# Train the model
+model = LogisticRegression()
+model.fit(X_train_vectorized, y_train)
 
-#train
-Inspirasi = training_set.loc[training_set['CATEGORY'] == 'Inspirasi']
-UnikAneh = training_set.loc[training_set['CATEGORY'] == 'UnikAneh']
-Travel = training_set.loc[training_set['CATEGORY'] == 'Travel']
-Tips = training_set.loc[training_set['CATEGORY'] == 'Tips']
-Hiburan = training_set.loc[training_set['CATEGORY'] == 'Hiburan']
-Olahraga = training_set.loc[training_set['CATEGORY'] == 'Olahraga']
-Trends = training_set.loc[training_set['CATEGORY'] == 'Trends']
-Block = training_set.loc[training_set['CATEGORY'] == 'Block']
-Entertainment = training_set.loc[training_set['CATEGORY'] == 'Entertainment']
+# Predict the transformed test documents
+predictions = model.predict(vect.transform(X_test))
 
-other = {}
-other['Inspirasi'] = list(Inspirasi['TITLE'])
-other['UnikAneh'] = list(UnikAneh['TITLE'])
-other['Travel'] = list(Travel['TITLE'])
-other['Tips'] = list(Tips['TITLE'])
-other['Hiburan'] = list(Hiburan['TITLE'])
-other['Olahraga'] = list(Olahraga['TITLE'])
-other['Trends'] = list(Trends['TITLE'])
-other['Block'] = list(Block['TITLE'])
-other['Entertainment'] = list(Entertainment['TITLE'])
+# These reviews are now correctly identified
+def run(judul):
+	# judul = 'Kata Plt Dirjen Hubla soal Suap Rp 20 Miliar yang Dibongkar KPK'
+	res = model.predict(vect.transform([judul]))
+	return res
 
-doc = cleansing(other)
+aplikasi = Flask(__name__)
 
-featuresets = [(document_features(d), c) for (d,c) in doc]
-random.shuffle(featuresets)
-
-train_feats, test_feats = featuresets[:int(len(featuresets)*0.8)], featuresets[int(len(featuresets)*0.8):]
-nb_classifier = NaiveBayesClassifier.train(train_feats)
-
-# print(accuracy(nb_classifier, test_feats))
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-	return render_template("index.html")
-
-@app.route("/predict", methods = ["POST"])
-def predict():
+@aplikasi.route("/prediksi", methods = ["POST"])
+def prediksi():
 	if request.method == "POST":
-		data = request.form['judul']
-		doc = document_features(data)
-		result = nb_classifier.classify(doc)
-
-		return result
-
+		judul = request.form['judul']
+		data = run(judul)
+		print(data)
+		return render_template("result.html", data = data)
 
 if __name__ == "__main__":
-   app.run()
+   aplikasi.run()
